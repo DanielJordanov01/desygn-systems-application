c428c9e88e565be3a48e5129478c509fa90078ea
DanielJordanov01 <daniel159696@gmail.com>
2022-04-23 21:15:11 +0100

    initial commit

---
+++ src/views/DemoView.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+//import { Demo } from "acmecorp-potted-plants-components-starter";
+
+export const DemoView = () => (
+  <div>
+    <h1>This is your brand new application scaffold!</h1>
+    {/* <Demo /> */}
+  </div>
+);
